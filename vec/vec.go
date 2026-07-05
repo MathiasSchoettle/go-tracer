f2b31c38aacd05df9a536d0e8b953ca0c25087b1
mschoettle <schoettlemathias@gmail.com>
2026-07-05 17:17:41 +0200

    updated vec

--- vec/vec.go
+++ vec/vec.go
@@ -36,12 +36,12 @@ func (v *Vector3) Length() float64 {
 	return math.Sqrt(v.x*v.x + v.y*v.y + v.z*v.z)
 }
 
-func (v *Vector3) Scale(s float64) Vector3 {
+func (v *Vector3) Scale(s float64) *Vector3 {
 	v.x *= s
 	v.y *= s
 	v.z *= s
 
-	return *v
+	return v
 }
 
 func Scaled(v Vector3, s float64) Vector3 {
@@ -52,12 +52,28 @@ func Scaled(v Vector3, s float64) Vector3 {
 	}
 }
 
-func (v *Vector3) Add(other Vector3) Vector3 {
-	v.x *= other.x
-	v.y *= other.y
-	v.z *= other.z
+func (v *Vector3) Divide(s float64) *Vector3 {
+	v.x /= s
+	v.y /= s
+	v.z /= s
 
-	return *v
+	return v
+}
+
+func Divided(v Vector3, s float64) Vector3 {
+	return Vector3{
+		v.x / s,
+		v.y / s,
+		v.z / s,
+	}
+}
+
+func (v *Vector3) Add(other Vector3) *Vector3 {
+	v.x += other.x
+	v.y += other.y
+	v.z += other.z
+
+	return v
 }
 
 func Added(first Vector3, other Vector3) Vector3 {
@@ -68,6 +84,22 @@ func Added(first Vector3, other Vector3) Vector3 {
 	}
 }
 
+func (v *Vector3) Subtract(other Vector3) *Vector3 {
+	v.x -= other.x
+	v.y -= other.y
+	v.z -= other.z
+
+	return v
+}
+
+func Subtracted(first Vector3, other Vector3) Vector3 {
+	return Vector3{
+		first.x - other.x,
+		first.y - other.y,
+		first.z - other.z,
+	}
+}
+
 func (v *Vector3) Normalize() *Vector3 {
 	length := v.Length()
 	v.x /= length
@@ -75,6 +107,15 @@ func (v *Vector3) Normalize() *Vector3 {
 	v.z /= length
 
 	return v
+}
+
+func Normalized(v Vector3) Vector3 {
+	length := v.Length()
+	return Vector3{
+		v.x / length,
+		v.y / length,
+		v.z / length,
+	}
 }
 
 func Dot(u Vector3, v Vector3) float64 {
