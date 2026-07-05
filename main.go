30df93ef87cad36a51de119426c428b0d5043cdb
mschoettle <schoettlemathias@gmail.com>
2026-07-05 17:18:21 +0200

    setup ray creation

--- main.go
+++ main.go
@@ -3,35 +3,77 @@ package main
 import (
 	"fmt"
 	img "go-raytrace/image"
+	. "go-raytrace/ray"
+	"go-raytrace/vec"
 	"os"
 )
 
 func main() {
 
-	width := 255
-	height := 255
+	// image
+	aspectRatio := 16.0 / 9.0
+	imageWidth := 400
+	imageHeight := int(float64(imageWidth) / aspectRatio)
+	if imageHeight < 1 {
+		imageHeight = 1
+	}
+	image := img.Create(imageWidth, imageHeight)
 
-	image := img.Create(width, height)
+	fmt.Printf("w %d, h %d \n", imageWidth, imageHeight)
 
-	for y := 0; y < height; y++ {
-		fmt.Printf("\rScanlines remaining: %d", height-y)
+	// camera
+	focalLength := 1.0
+	viewportHeight := 2.0
+	viewportWidth := viewportHeight * (float64(imageWidth) / float64(imageHeight))
+	cameraCenter := vec.Of(0, 0, 0)
 
-		for x := 0; x < width; x++ {
+	viewportU := vec.Of(viewportWidth, 0, 0)
+	viewportV := vec.Of(0, -viewportHeight, 0)
 
-			r := float64(y) / float64(width-1)
-			g := float64(x) / float64(height-1)
-			b := 0.0
+	pixelDeltaU := vec.Scaled(viewportU, 1.0/float64(imageWidth))
+	pixelDeltaV := vec.Scaled(viewportV, 1.0/float64(imageHeight))
 
-			ir := int(255.999 * r)
-			ig := int(255.999 * g)
-			ib := int(255.999 * b)
+	viewportUpperLeft := vec.Subtracted(cameraCenter, vec.Of(0, 0, focalLength))
+	viewportUpperLeft.Subtract(vec.Scaled(viewportU, 0.5)).Subtract(vec.Scaled(viewportV, 0.5))
+
+	pixel00Loc := vec.Added(viewportUpperLeft, vec.Scaled(vec.Added(pixelDeltaU, pixelDeltaV), 0.5))
+
+	fmt.Printf("%f, %f, %f\n", viewportU.X(), viewportU.Y(), viewportU.Z())
+
+	for y := 0; y < imageHeight; y++ {
+		fmt.Printf("\rScanlines remaining: %d", imageHeight-y)
+
+		for x := 0; x < imageWidth; x++ {
+			deltaU := vec.Scaled(pixelDeltaU, float64(x))
+			deltaV := vec.Scaled(pixelDeltaV, float64(y))
+			pixelCenter := vec.Added(pixel00Loc, deltaU)
+			pixelCenter.Add(deltaV)
+
+			rayDir := vec.Subtracted(pixelCenter, cameraCenter)
+			ray := Ray{Origin: cameraCenter, Direction: rayDir}
+
+			color := rayColor(ray)
+
+			ir := int(255.999 * color.X())
+			ig := int(255.999 * color.Y())
+			ib := int(255.999 * color.Z())
 
 			image.Set(x, y, [3]int{ir, ig, ib})
 		}
 	}
 
+	fmt.Println("\n done")
+
 	err := os.WriteFile("image.ppm", image.ToPPM(), 0777)
 	if err != nil {
 		return
 	}
+}
+
+func rayColor(ray Ray) vec.Vector3 {
+	direction := vec.Normalized(ray.Direction)
+	a := 0.5 * (direction.Y() + 1.0)
+	first := vec.Scaled(vec.Of(1, 1, 1), 1.0-a)
+	second := vec.Scaled(vec.Of(0.5, 0.7, 0.2), a)
+	return vec.Added(first, second)
 }
